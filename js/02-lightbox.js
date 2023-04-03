489a26666ae84e29be662690a913164f9c3f298f
Ishchenko-Oleksii <110259763+Ishchenko-Oleksii@users.noreply.github.com>
2023-04-03 23:53:10 +0300

    all_task

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+const   creatMark = creatMarkUp();
+function creatMarkUp() {
+    const markup = galleryItems.reduce((pictures, { preview, original, description }) => {
+        pictures += `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+        return pictures;
+    }, "");
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+const lightBoxGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    close: true,
+});
+
+console.log(galleryItems);
